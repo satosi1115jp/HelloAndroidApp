8a9d219d66042e94fc0f2d4f0443ba3397746546
satosi1115jp <m13047st@g.edogawa-u.ac.jp>
2021-02-06 23:38:23 +0900

    Initial commit 最初のコミット

---
+++ app/src/main/java/jp/techacademy/Satoshi/Tanaka/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.Satoshi.Tanaka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
